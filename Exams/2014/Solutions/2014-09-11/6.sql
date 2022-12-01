78d81ee6ed5e9470493eda201955838d0d9c8e87
andy489 <stoev.andy@gmail.com>
2022-12-01 14:06:52 +0200

    Update

--- Exams/2014/Solutions/2014-09-11/6.sql
+++ Exams/2014/Solutions/2014-09-11/6.sql
@@ -1,5 +1,5 @@
 -- 2014-09-11
-USE Ships
+USE Ships;
 
 -- 6.1 Б)
 SELECT DISTINCT Ships.name
@@ -8,10 +8,10 @@ WHERE launched = YEAR(date);
 
 -- 6.2. Д)
 SELECT DISTINCT country, (SELECT COUNT(*)
-				          FROM Classes c2 JOIN Ships s ON c2.class = s.class
-						  JOIN Outcomes o on s.name = o.ship
-                          WHERE c2.country = c.country AND result = 'sunk') AS cnt_sink
+FROM Classes c2 JOIN Ships s ON c2.class = s.class
+JOIN Outcomes o on s.name = o.ship
+WHERE c2.country = c.country AND result = 'sunk') AS cnt_sink
 FROM Classes c
 WHERE (SELECT COUNT(*)
-	   FROM Classes c2 JOIN Ships s ON c2.class = s.class
-       WHERE c2.country = c.country) <= 3;
+FROM Classes c2 JOIN Ships s ON c2.class = s.class
+WHERE c2.country = c.country) <= 3;
