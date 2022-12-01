e2bbaefab3e4f53abcb1d4491be49ecd3073c806
andy489 <stoev.andy@gmail.com>
2022-12-01 15:06:48 +0200

    Update

--- Exams/2016/Solutions/2016-07-12/4.sql
+++ Exams/2016/Solutions/2016-07-12/4.sql
@@ -4,10 +4,10 @@ USE Movies
 -- 4.1 A)
 SELECT t.name, title
 FROM movie m JOIN ( SELECT name, cert#
-                    FROM Movieexec
-                    WHERE cert# IN (SELECT producerc#
-                                    FROM movie      
-                                    WHERE title='Prety Woman')) T
+FROM Movieexec
+WHERE cert# IN (SELECT producerc#
+FROM movie      
+WHERE title='Prety Woman')) T
 ON m.producerc#=t.cert#
 
 -- 4.2. Г)
@@ -16,6 +16,6 @@ FROM Movie m JOIN StarsIn ON title = movieTitle AND year = movieYear
 WHERE studioName IS NOT NULL
 GROUP BY studioName, starName
 HAVING COUNT(*) >= ALL (SELECT COUNT(*)
-						FROM Movie, StarsIn
-						WHERE title = movieTitle AND year = movieYear AND studioName = m.studioName
-						GROUP BY studioName, starName)
+FROM Movie, StarsIn
+WHERE title = movieTitle AND year = movieYear AND studioName = m.studioName
+GROUP BY studioName, starName)
