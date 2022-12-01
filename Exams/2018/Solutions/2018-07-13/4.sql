a7d6f13929552881daff20b91e6530e0fb6b0fbf
andy489 <stoev.andy@gmail.com>
2022-12-01 16:18:18 +0200

    Update

--- Exams/2018/Solutions/2018-07-13/4.sql
+++ Exams/2018/Solutions/2018-07-13/4.sql
@@ -1,5 +1,5 @@
 -- 2018-07-13, 4
-USE Movie
+USE Movies;
 
 -- 4.1. EXCLUDING STUDIOS WITH 0 MOVIES
 SELECT studioName, MIN(year) AS first_movie_year, MAX(year) AS last_movie_year, COUNT(title) AS cnt_movies
@@ -19,6 +19,6 @@ FROM StarsIn LEFT JOIN MovieStar ON starName = name
 WHERE gedner = 'F'
 GROUP BY name
 HAVING COUNT(movieTitle) >= ALL(SELECT COUNT(movieTitle)
-								FROM MovieStar LEFT JOIN StarsIn ON name = starName
-								WHERE gender = 'F'
-								GROUP BY name);
+FROM MovieStar LEFT JOIN StarsIn ON name = starName
+WHERE gender = 'F'
+GROUP BY name);
--- Exams/2018/Solutions/2018-09-10/4.sql
+++ Exams/2018/Solutions/2018-09-10/4.sql
@@ -3,35 +3,35 @@ USE Ships;
 
 -- 4.1. 
 SELECT C.class, MIN(YEAR(B.date)) AS firts_battle_year,    
-				MAX(YEAR(B.date)) AS last_battle_year, 
-                COUNT(DISTINCT B.name) AS cnt_battles 
+MAX(YEAR(B.date)) AS last_battle_year, 
+COUNT(DISTINCT B.name) AS cnt_battles 
 FROM Classes C LEFT JOIN Ships S ON C.class = S.class 
-			   LEFT JOIN Outcomes O ON S.name = O.ship 
-               LEFT JOIN Battles B ON O.battle = B.name
+LEFT JOIN Outcomes O ON S.name = O.ship 
+LEFT JOIN Battles B ON O.battle = B.name
 WHERE C.class LIKE "N%"
 GROUP BY C.class;
 
 -- 4.2. version 1
 SELECT O.battle, COUNT(*) AS cnt_bb
 FROM Classes C INNER JOIN Ships S ON C.class = S.class
-	INNER JOIN Outcomes O ON S.name = O.ship
-	WHERE C.type LIKE 'bb'
-	GROUP BY O.battle
-	HAVING COUNT(*) > (SELECT COUNT(*)
-					   FROM Classes C1 INNER JOIN Ships S1 ON C1.class = S1.class
-					   INNER JOIN Outcomes O1 ON S1.name = O1.ship
-                       WHERE C1.type LIKE 'bc' AND O1.battle = O.battle);
+INNER JOIN Outcomes O ON S.name = O.ship
+WHERE C.type LIKE 'bb'
+GROUP BY O.battle
+HAVING COUNT(*) > (SELECT COUNT(*)
+FROM Classes C1 INNER JOIN Ships S1 ON C1.class = S1.class
+INNER JOIN Outcomes O1 ON S1.name = O1.ship
+WHERE C1.type LIKE 'bc' AND O1.battle = O.battle);
 -- 4.2. version 2 
 SELECT TABLE_BB.battle, TABLE_BB.cnt_bb
 FROM (SELECT O.battle, COUNT(*) AS cnt_bb
-		FROM Classes C INNER JOIN Ships S ON C.class = S.class
-			INNER JOIN Outcomes O ON S.name = O.ship
-		WHERE C.type LIKE 'bb'
-		GROUP BY O.battle) TABLE_BB
+FROM Classes C INNER JOIN Ships S ON C.class = S.class
+INNER JOIN Outcomes O ON S.name = O.ship
+WHERE C.type LIKE 'bb'
+GROUP BY O.battle) TABLE_BB
 LEFT JOIN (SELECT O.battle, COUNT(*) AS cnt_bc
-		FROM Classes C INNER JOIN Ships S ON C.class = S.class
-			INNER JOIN Outcomes O ON S.name = O.ship
-		WHERE C.type LIKE 'bc'
-		GROUP BY O.battle) TABLE_BC
+FROM Classes C INNER JOIN Ships S ON C.class = S.class
+INNER JOIN Outcomes O ON S.name = O.ship
+WHERE C.type LIKE 'bc'
+GROUP BY O.battle) TABLE_BC
 ON TABLE_BB.battle = TABLE_BC.battle
 WHERE TABLE_BB.cnt_bb >= TABLE_BC.cnt_bc;
