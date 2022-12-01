2e82ce71cf1e77902cad1afeb04452e5386a5074
andy489 <stoev.andy@gmail.com>
2022-12-01 14:36:32 +0200

    Update

--- Exams/2015/Solutions/2015-09-10/4.sql
+++ Exams/2015/Solutions/2015-09-10/4.sql
@@ -4,11 +4,11 @@ USE Movies
 -- 4.1 Б)
 SELECT *
 FROM (SELECT name, address
-     FROM MovieStar
-     WHERE gender = 'F'
-     UNION
-     SELECT name, address
-     FROM Studio) T
+FROM MovieStar
+WHERE gender = 'F'
+UNION
+SELECT name, address
+FROM Studio) T
 WHERE T.address LIKE '%Sofia%'
 ORDER BY T.address;
 
@@ -17,8 +17,8 @@ SELECT name, address, AVG(length) AS avgLength
 FROM Studio
 LEFT JOIN Movie ON name = studioName
 WHERE NAME NOT IN (SELECT studioName
-				   FROM Movie
-				   WHERE inColor = 'n'
-                   GROUP BY studioName
-                   HAVING COUNT(*) > 3)
+FROM Movie
+WHERE inColor = 'n'
+GROUP BY studioName
+HAVING COUNT(*) > 3)
 GROUP BY name, address;
